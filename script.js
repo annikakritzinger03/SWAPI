28135658149c626130f5382dd69be8161af336fa
Annika Kritzinger <annika.kritzinger@gmail.com>
2023-11-18 20:35:20 +0200

    Displaying and formatting results

--- script.js
+++ script.js
@@ -9,48 +9,116 @@ class Character{
         this.mass = mass;
         this.species = species;
 
-        this.traningLvl;
-        this.weapon;
-        this.skill;
+        this.traningLvl = 0;
+        this.weapon = 0;
+        this.skill = 0;
+
+        this.score = 0;
+    }
+
+    CalculateScore(){
+        this.score = parseInt(this.species) + parseInt(this.trainingLvl) + 
+            parseInt(this.weapon) + parseInt(this.skill);
     }
 }
 
 const trainingLevels = {
-    "Sith Lord" : 1,
-    "Jedi Master" : 2,
-    "Jedi Knight" : 3,
-    "Rebellion Leader" : 4,
-    "Wookiee Warrior" : 5,
-    "Gungan Warrior" : 6,
-    "Crime Lord" : 7,
-    "Pilot" : 8,
-    "Astromech Droid" : 9,
-    "Protocol Droid" : 10
+    "Sith Lord" : 10,
+    "Jedi Master" : 9,
+    "Jedi Knight" : 8,
+    "Rebellion Leader" : 7,
+    "Wookiee Warrior" : 6,
+    "Gungan Warrior" : 5,
+    "Crime Lord" : 4,
+    "Pilot" : 3,
+    "Astromech Droid" : 2,
+    "Protocol Droid" : 1
 };
 
 const weapons = {
-    "Lightsaber" : 1,
-    "Bowcaster" : 2,
+    "Lightsaber" : 5,
+    "Bowcaster" : 4,
     "Blaster pistol" : 3,
-    "Tools and gadgets" : 4,
-    "None" : 5
+    "Tools and gadgets" : 2,
+    "None" : 1
 };
 
 const skills = {
-    "Using the dark side of the force" : 1,
-    "Using the force (highly proficient)" : 2,
-    "Using the force (proficient)" : 3,
-    "Using the force (capable)" : 4,
-    "Strong and physically powerful" : 5,
+    "Using the dark side of the force" : 11,
+    "Using the force (highly proficient)" : 10,
+    "Using the force (proficient)" : 9,
+    "Using the force (capable)" : 8,
+    "Strong and physically powerful" : 7,
     "Expert marksman" : 6,
-    "Mind control and force persuasion" : 7,
-    "Unintentionally lucky" : 8,
-    "Manipulative and cunning" : 9,
-    "Computer hacking" : 10,
-    "Fluent in 6+ million languages" : 11
+    "Mind control and force persuasion" : 5,
+    "Unintentionally lucky" : 4,
+    "Manipulative and cunning" : 3,
+    "Computer hacking" : 2,
+    "Fluent in 6+ million languages" : 1
 };
 
-function Compare()
+const speciesList = {
+    "Droid" : 6,
+    "Wookie" : 5,
+    "Yoda's species" : 4,
+    "Human" : 3,
+    "Gungan" : 2,
+    "Hutt" : 1
+};
+
+// from https://www.geeksforgeeks.org/how-to-get-a-key-in-a-javascript-object-by-its-value/
+function getKeyByValue(object, value){
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function ShowAttributeComparison(value1, value2, component1, component2){
+    if(value1 > value2){
+        component1.style = "border: 8px solid rgb(34, 155, 34)";
+        component2.style = "border: 8px solid rgb(177, 23, 23)";
+    }
+    else if(value1 < value2){
+        component1.style = "border: 8px solid rgb(177, 23, 23)";
+        component2.style = "border: 8px solid rgb(34, 155, 34)";
+    }
+    else{
+        component1.style = "border: 8px solid blue";
+        component2.style = "border: 8px solid blue";
+    }
+}
+
+async function FormatResults(){
+    ShowAttributeComparison(parseInt(document.getElementById("height1").value), 
+        parseInt(document.getElementById("height2").value), 
+        document.getElementById("height1p"),
+        document.getElementById("height2p"));
+
+    ShowAttributeComparison(parseInt(document.getElementById("mass1").value), 
+    parseInt(document.getElementById("mass2").value), 
+        document.getElementById("mass1p"),
+        document.getElementById("mass2p"));
+
+    ShowAttributeComparison(speciesList[document.getElementById("species1").value], 
+        speciesList[document.getElementById("species2").value], 
+        document.getElementById("species1p"),
+        document.getElementById("species2p"));
+
+    ShowAttributeComparison(trainingLevels[document.getElementById("training1").value], 
+        trainingLevels[document.getElementById("training2").value], 
+        document.getElementById("training1p"),
+        document.getElementById("training2p"));
+
+    ShowAttributeComparison(weapons[document.getElementById("weapon1").value], 
+        weapons[document.getElementById("weapon2").value], 
+        document.getElementById("weapon1p"),
+        document.getElementById("weapon2p"));
+
+    ShowAttributeComparison(skills[document.getElementById("skill1").value], 
+    skills[document.getElementById("skill2").value], 
+        document.getElementById("skill1p"),
+        document.getElementById("skill2p"));
+}
+
+async function Compare()
 {
     const selectedChar1 = document.getElementById("character1");
     const selectedChar2 = document.getElementById("character2");
@@ -61,24 +129,95 @@ function Compare()
         document.getElementById("result").innerHTML = `${selectedOption1.innerHTML} CANNOT BATTLE AGAINST THEMSELF!`;
     }
 
-    const char1Score = GetCharacterScore(selectedOption1, 1);
-    const char2Score = GetCharacterScore(selectedOption2, 2);
+    const char1Score = await GetCharacterScore(selectedOption1, 1);
+    const char2Score = await GetCharacterScore(selectedOption2, 2);
+    document.getElementById("result1").innerHTML = "SCORE:  " + char1Score;
+    document.getElementById("result2").innerHTML = "SCORE:  " + char2Score;
+
+    await FormatResults();
     
     let winner = (char1Score > char2Score) ? selectedOption1 : selectedOption2;
     document.getElementById("result").innerHTML = `${winner.innerHTML} WILL WIN IN BATTLE!`;
 }
 
-function GetCharacterScore(selectedOption, charNum){
-    let charScore = 0;
+async function GetCharacterScore(selectedOption, charNum){
     let person = selectedOption.value;
     let request = `https://swapi.dev/api/people/${person}/`;
 
-    let character = GetData(request, charNum);
-        // .then(() => {
-    //     let speciesRequest = document.getElementById(`species${charNum}`).value;
-    //     //Getting species (third field)
-    //     GetSpecies(speciesRequest); 
-    // }); 
+    let character = await GetData(request, charNum);
+
+    switch(character.name){
+        case "Luke Skywalker":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Jedi Knight"];
+            character.weapon = weapons.Lightsaber;
+            character.skill = skills["Using the force (proficient)"];
+            break;
+        case "C-3PO":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Protocol Droid"];
+            character.weapon = weapons.None;
+            character.skill = skills["Fluent in 6+ million languages"];
+            break;
+        case "R2-D2":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Astromech Droid"];
+            character.weapon = weapons["Tools and gadgets"];
+            character.skill = skills["Computer hacking"];
+            break;
+        case "Darth Vader":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Sith Lord"];
+            character.weapon = weapons.Lightsaber;
+            character.skill = skills["Using the dark side of the force"];
+            break;
+        case "Leia Organa":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Rebellion Leader"];
+            character.weapon = weapons["Blaster pistol"];
+            character.skill = skills["Using the force (capable)"];
+            break;
+        case "Obi-Wan Kenobi":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Jedi Master"];
+            character.weapon = weapons.Lightsaber;
+            character.skill = skills["Mind control and force persuasion"]
+            break;
+        case "Chewbacca":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Wookiee Warrior"];
+            character.weapon = weapons.Bowcaster;
+            character.skill = skills["Strong and physically powerful"];
+            break;                      
+        case "Han Solo":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels.Pilot;
+            character.weapon = weapons["Blaster pistol"];
+            character.skill = skills["Expert marksman"];
+            break;
+        case "Jabba Desilijic Tiure":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Crime Lord"]
+            character.weapon = weapons.None;
+            character.skill = skills["Manipulative and cunning"];
+            break;
+        case "Yoda":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Jedi Master"];
+            character.weapon = weapons.Lightsaber;
+            character.skill = skills["Using the force (highly proficient)"];
+            break;
+        case "Jar Jar Binks":
+            character.species = speciesList[character.species];
+            character.trainingLvl = trainingLevels["Gungan Warrior"];
+            character.weapon = weapons.None;
+            character.skill = skills["Unintentionally lucky"];
+            break;     
+    }
+
+    console.log(character);
+
+    let charScore = character.CalculateScore();
 
     let height = document.getElementById(`height${charNum}`);
     let mass = document.getElementById(`mass${charNum}`);
@@ -87,115 +226,47 @@ function GetCharacterScore(selectedOption, charNum){
     let weapon = document.getElementById(`weapon${charNum}`);
     let skill = document.getElementById(`skill${charNum}`);
 
-    switch(true){
-        case (character.name == "Luke Skywalker"):
-            character.trainingLvl = trainingLevels["Jedi Knight"];
-            character.weapon = weapons.Lightsaber;
-            character.skill = skills["Using the force (proficient)"];
-            break;
-        case (character.name == "C-3PO"):
-            character.trainingLvl = trainingLevels["Protocol Droid"];
-            character.weapon = weapons.None;
-            character.skill = skills["Fluent in 6+ million languages"];
-            break;
-        case (character.name == "R2-D2"):
-            character.trainingLvl = trainingLevels["Astromech Droid"];
-            character.weapon = weapons["Tools and gadgets"];
-            character.skill = skills["Computer hacking"];
-            break;
-        case (character.name == "Darth Vader"):
-            character.trainingLvl = trainingLevels["Sith Lord"];
-            character.weapon = weapons.Lightsaber;
-            character.skill = skills["Using the dark side of the force"];
-            break;
-        case (character.name == "Leia Organa"):
-            character.trainingLvl = trainingLevels["Rebellion Leader"];
-            character.weapon = weapons["Blaster pistol"];
-            character.skill = skills["Using the force (capable)"];
-            break;
-        case (character.name == "Obi-Wan Kenobi"):
-            character.trainingLvl = trainingLevels["Jedi Master"];
-            character.weapon = weapons.Lightsaber;
-            character.skill = skills["Mind control and force persuasion"]
-            break;
-        case (character.name == "Chewbacca"):
-            character.trainingLvl = trainingLevels["Wookiee Warrior"];
-            character.weapon = weapons.Bowcaster;
-            character.skill = skills["Strong and physically powerful"];
-            break;                      
-        case (character.name == "Han Solo"):
-            character.trainingLvl = trainingLevels.Pilot;
-            character.weapon = weapons["Blaster pistol"];
-            character.skill = skills["Expert marksman"];
-            break;
-        case (character.name == "Jabba Desilijic Tiure"):
-            character.trainingLvl = trainingLevels["Crime Lord"]
-            character.weapon = weapons.None;
-            character.skill = skills["Manipulative and cunning"];
-            break;
-        case (character.name == "Yoda"):
-            character.trainingLvl = trainingLevels["Jedi Master"];
-            character.weapon = weapons.Lightsaber;
-            character.skill = skills["Using the force (highly proficient)"];
-            break;
-        case (character.name == "Jar Jar Binks"):
-            character.trainingLvl = trainingLevels["Gungan Warrior"];
-            character.weapon = weapons.None;
-            character.skill = skills["Unintentionally lucky"];
-            break;     
-    }
-
-    let trainingScore = trainingLevels[character.trainingLvl];
-    let weaponScore = weapons[character.weapon];
-    let skillScore = skills[character.skill];
-
     height.value = character.height;
     mass.value = character.mass;
-    species.value = character.species;
-    trainingLvl.value = character.training;
-    weapon.value = character.weapon;
-    skill.value = character.skill;
+    species.value = getKeyByValue(speciesList, character.species);
+    trainingLvl.value = getKeyByValue(trainingLevels, character.trainingLvl);
+    weapon.value = getKeyByValue(weapons,character.weapon);
+    skill.value = getKeyByValue(skills,character.skill);
   
-    return charScore;
+    return character.score;
 }
 
-function GetData(URL, charNum){
-    let character;
-
-    fetch(URL)
+async function GetData(URL, charNum){
+    return await fetch(URL)
     .then((response) => {
         if(!response.ok) throw new Error(response.statusText);
         return response.json();
-    }).then((data) => {
-        //Can query the entire object or just specific parts of it
-        // height.value = data.height;
-        // mass.value = data.mass;
+    }).then(async (data) => {
         console.log(data);
         const species = data.species;
-        const speciesName = GetSpecies(species);
+        const speciesName = await GetSpecies(species);
 
         character = new Character(data.name, data.height, data.mass, speciesName);
-    }).catch(console.error);
-
-    return character;
+        return character;
+    }).catch(error => console.log(error));
 }
 
-function GetSpecies(speciesURL){
+async function GetSpecies(speciesURL){
     let speciesName;
 
     if(speciesURL.length == 0){
         speciesName = "Human";
+        return speciesName;
     }
     else{         
-        fetch(speciesURL)
+        return await fetch(speciesURL)
         .then((speciesResponse) => {
             if(!speciesResponse.ok) throw new Error(speciesResponse.statusText);
             return speciesResponse.json();
         }).then((speciesData) => {
             console.log(speciesData);
             speciesName = speciesData.name;
-        }).catch(console.error);
+            return speciesName;
+        }).catch(error => console.log(error));
     }
-
-    return speciesName;
 }
